0f4c26f2e38ea3fbee7ae6ad228f2f9ad01065aa
Dan Kolz <dan@emergentideas.com>
2024-03-25 21:39:12 -0500

    use common methods

--- textarea-value-injector.js
+++ textarea-value-injector.js
@@ -1,3 +1,11 @@
+const {
+	evalFunction
+	, attributeEscapes
+	, fetchValue
+	, isOrContains
+	, escForRegex
+	, escapeAttributeValue
+} = require('value-injector-common')
 
 
 let nameAttrPattern = /\sname=["'](.*?)["']/i
@@ -5,42 +13,6 @@ let valAttrPattern = /\svalue=["'](.*?)["']/i
 let typeAttrPattern = /\stype=["'](.*?)["']/i
 let textareaPattern = /(<textarea[\w\W]*?textarea\w*>)/im
 let selectedAttrPattern = /\sselected(=["'](.*?)["'])?/i
-
-
-let evalFunction = new Function('data',
-	`with (data.context) {
-		try {
-			return eval(data.expression);
-		} catch (e) {
-			return null;
-		}
-	}`
-)
-
-function fetchValue(obj, path) {
-	return evalFunction.call(this, {
-		context: obj
-		, expression: path
-	})
-}
-
-function escForRegex(val) {
-	if(val && val.replace) {
-		return val.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&")
-	}
-	else {
-		return val;
-	}
-}
-
-function isOrContains(target, possible) {
-	if(typeof possible == 'array') {
-		possible.includes(target)
-	}
-	else {
-		return target == possible
-	}
-}
 
 
 let injectValues = function(text, values) {
